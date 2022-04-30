3337e700395bf86ca1c2f6bbb07898ddca424779
Nirav01 <nirav.ramidi@ucdconnect.ie>
2022-05-01 00:13:26 +0100

    tweets header file modified

---
+++ tweets.h
@@ -0,0 +1,13 @@
+
+
+#include "twitter_create.h"
+
+#ifndef PROJECT2_TWEETS_H
+#define PROJECT2_TWEETS_H
+
+#endif //PROJECT2_TWEETS_H
+
+
+void posttweet(twitter *ts, user *ptr);
+
+void getNewsFeed(twitter *ts, user *currUser);
