d1445b6eb43abbe2c9e42128d1b21624149b7c12
Nirav01 <nirav.ramidi@ucdconnect.ie>
2022-05-01 09:53:10 +0100

    tweets c file modified

--- tweets.c
+++ tweets.c
@@ -25,3 +25,35 @@ void posttweet(twitter *ts, user* ptr)
         ts->tweetheadptr= currptr;
     }
 }
+void getNewsFeed(twitter *ts, user *currUser)
+{
+    tweetPtr currptr; //temp ptr
+    int count = 0;
+    currptr = ts->tweetHeadPtr;
+    while(currptr != NULL)
+    {
+        if(currUser->num_following>0) {
+            for (int i = 0; i < currUser->num_following; i++) {
+                if (strcasecmp(currptr->user, currUser->following[i]) == 0 ||strcasecmp(currptr->user, currUser->username) == 0)
+                {
+                    count++;
+                    printf("%s\n", currptr->msg);
+                    //                printf("%d\n", currptr->id);
+                }
+            }
+        }
+        else
+        {
+            if(strcasecmp(currptr->user, currUser->username)==0)
+            {
+                count++;
+                printf("%s\n", currptr->msg);
+            }
+        }
+        if(count==10)
+        {
+            break;
+        }
+        currptr = currptr->nextPtr;
+    }
+}
