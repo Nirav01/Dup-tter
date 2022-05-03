5974f8bcb26606ba2e22ecb786e1e102f2339d2f
Nirav01 <nirav.ramidi@ucdconnect.ie>
2022-05-03 15:37:27 +0100

    Functions.c file modified

--- Functions.c
+++ Functions.c
@@ -67,3 +67,109 @@ void delete_user(twitter *ts, user *curruser)
     sub_delete(ts, curruser);
     free(curruser);
 }
+void sub_delete(twitter *ts, user *curruser)
+{
+    char tmpuser[USR_LENGTH];
+    for(int i = 0; i < curruser->num_followers; i++){
+        strcpy(tmpuser,curruser->followers[i]);
+        userPtr tmpptr = ts->headPtr;
+        while(strcasecmp(tmpptr->username,tmpuser)!=0)
+        {
+            tmpptr = tmpptr->nextptr;
+        }
+        int k = 0;
+        while(strcasecmp(tmpptr->following[k],curruser->username)!=0){
+            k++;
+        }
+        for(int o = k+1; o < tmpptr->num_following; o++){
+            strcpy(tmpptr->following[k],tmpptr->following[o]);
+            k++;
+        }
+        tmpptr->num_following--;
+    }
+    for(int i = 0; i < curruser->num_following; i++){
+        strcpy(tmpuser,curruser->following[i]);
+        userPtr tmpptr = ts->headPtr;
+        while(strcasecmp(tmpptr->username,tmpuser)!=0){
+            tmpptr = tmpptr->nextptr;
+        }
+        int k = 0;
+        while(strcasecmp(tmpptr->followers[k],curruser->username)!=0){
+            k++;
+        }
+        for(int o = k+1; o < tmpptr->num_followers; o++){
+            strcpy(tmpptr->followers[k],tmpptr->followers[o]);
+            k++;
+        }
+        tmpptr->num_followers--;
+    }
+
+    if(ts->tweetHeadPtr == NULL){
+        return;
+    }
+    tweetPtr currtweet = ts->tweetHeadPtr;
+    tweetPtr prevtweet = ts->tweetHeadPtr->nextPtr;
+    while(currtweet != NULL){
+        if(strcasecmp(curruser->username,currtweet->user) == 0){
+            if(currtweet == ts->tweetHeadPtr){
+                ts->tweetHeadPtr = currtweet->nextPtr;
+                free(currtweet);
+                currtweet = ts->tweetHeadPtr->nextPtr;
+                continue;
+            }
+            prevtweet->nextPtr = currtweet->nextPtr;
+            free(currtweet);
+            currtweet = prevtweet->nextPtr;
+            continue;
+        }
+        prevtweet = currtweet;
+        currtweet = currtweet->nextPtr;
+    }
+}
+void unfollow(twitter *ts, user *ptr)
+{
+    char name[USR_LENGHT];
+    char null[USR_LENGHT];
+    Userptr currptr;
+    currptr = ts->headptr;
+    for(int j = 0; j < USR_LENGHT; j++)
+    {
+        null[j] = '\0';
+    }
+    printf("\nEnter username of the user you would like to unfollow:\n");
+    fflush(stdin);
+    fgets(name,USR_LENGHT,stdin);
+    if(name[strlen(name) - 1] == '\n') {
+        name[strlen(name) - 1] = '\0';
+    }
+    int i, check = 0;
+    for(i = 0; ptr->num_following; i++){
+        if(strcmp(ptr->following[i], name) == 0)
+        {
+            strcpy(ptr->following[i], null);
+            ptr->num_following--;
+            check = 1;
+            break;
+        }
+    }
+    if(check == 0)
+    {
+        printf("Error, entered user not found.\n"
+               "You do not follow this User.\n");
+        return;
+    }
+    while(currptr->nextptr != NULL)
+    {
+        if(strcasecmp(currptr->username, name) == 0){
+            break;
+        }
+        currptr = currptr->nextptr;
+    }
+    for(int k = 0; k < currptr->num_followers; k++){
+        if(strcmp(currptr->followers[i], ptr->username) == 0){
+            strcpy(currptr->followers[i], null);
+            currptr->num_followers--;
+            break;
+        }
+    }
+}
